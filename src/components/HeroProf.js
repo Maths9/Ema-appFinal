238cd9d28c23fa6d932f2099295988be61f696e9
Maths9 <mbl@discente.ifpe.edu.br>
2021-12-03 13:00:21 -0300

    03/12

--- src/components/HeroProf.js
+++ src/components/HeroProf.js
@@ -1,12 +1,34 @@
-import React from "react";
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { BrowserRouter, BrowserRouter as Router, Route } from 'react-router-dom';
+import HomeAluno from '../Pages/HomeAluno';
+import Atividades from '../Pages/Atividades';
+import Boletim from '../Pages/Boletim';
 
-const  HeroProf = ({handleLogout}) =>{
-    return(
-        <div>
-            <h1>oi</h1>
-            <button onClick={handleLogout}>Logout</button>
-        </div>
+const Hero = ({ handleLogout }) => {
+
+    const [showNav, setShowNav] = useState(false)
+
+    return (
+        <section className="hero">
+            <Router>
+                <nav>
+                    <GiHamburgerMenu className="hamburguer" onClick={() => setShowNav(!showNav)} />
+
+                    <button onClick={handleLogout}>Logout</button>
+                </nav>
+                    <Navbar show={showNav} />
+                
+
+                <div className="MainHero">
+                <Route path="/HomeAluno" component={HomeAluno} />
+                <Route path="/Atividades" component={Atividades} />
+                <Route path="/Boletim" component={Boletim} />
+                </div>
+            </Router>
+        </section>
     )
 }
 
-export default HeroProf;
+export default Hero;
